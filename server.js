ccce7cf9d24048963389688c6d8ebd23007ae785
Patty C <patrickcorcoran10@gmail.com>
2020-03-03 18:53:44 -0600

    server updates

--- server.js
+++ server.js
@@ -14,20 +14,6 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, ".client/build/index.html"));
 });
 app.use(routes);
-sgMail.setApiKey(
-  "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"
-);
-// app.get("/api/send-email", (req, res) => {
-//   console.log("hit");
-//   const msg = {
-//     to: "patrickcorcoran10@gmail.com",
-//     from: "test@example.com",
-//     subject: "Sending with Twilio SendGrid is Fun",
-//     text: "and easy to do anywhere, even with Node.js",
-//     html: "<strong>and easy to do anywhere, even with Node.js</strong>"
-//   };
-//   sgMail.send(msg);
-// });
 
 app.listen(PORT, function() {
   console.log("We are Showing our Work on PORT", PORT);
